f8cb3cf38456e423a81b6317e366d5388859d0a5
auda-cz <55245600+auda-cz@users.noreply.github.com>
2024-02-05 14:27:14 -0500

    minor fix

--- src/library/noise.jl
+++ src/library/noise.jl
@@ -12,7 +12,6 @@ NoiseType = -1 (flicker) by default
 
 """
 power_law_noise(len::Integer) = power_law_noise(len, -1)
-power_law_noise(len::Integer, NoiseType) = power_law_noise(len, NoiseType)
 
 function power_law_noise(len::Integer, NoiseType)
     white_noise = randn(len)
@@ -22,8 +21,8 @@ function power_law_noise(len::Integer, NoiseType)
     # Avoid division by zero for the first element
     power_law_spectrum = white_noise_fft .* (frequencies .+ 1e-10).^ (NoiseType / 2)
     # Inverse FFT to get noise in time domain
-    power_law_noise = real(FFTW.irfft(power_law_spectrum, len))
+    noises = real(FFTW.irfft(power_law_spectrum, len))
     # Center and normalize the noise
-    power_law_noise = (power_law_noise .- mean(power_law_noise)) ./ std(power_law_noise)
-    return power_law_noise
+    noises = (noises .- mean(noises)) ./ std(noises)
+    return noises
 end
